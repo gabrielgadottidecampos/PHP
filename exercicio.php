8a20f2bdc9388a56c46ce47e7e096267269b81f9
Gabriel <gabriel-gadotti@hotmail.com>
2021-03-05 21:44:01 -0300

    versão 1.2

---
+++ exercicio.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap"
+          rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+    <link rel="stylesheet" href="assets/css/exercicio.css">
+    <meta charset="UTF-8">
+    <title>curso</title>
+</head>
+<body class="exercicio">
+<header class="cabecalho">
+    <h1>Curso PHP</h1>
+    <h2>Indice dos Exercícios</h2>
+</header>
+<nav class="navegacao">
+    <a href="#" class="verde">Sem formatação</a>
+    <a href="index.php" class="vermelho">Voltar</a>
+</nav>
+<main class="principal">
+    <div class="conteudo">
+
+    </div>
+</main>
+<footer class="rodape">
+    Gadotti© <?= date('Y')?>
+</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap"
+          rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+    <meta charset="UTF-8">
+    <title>curso</title>
+</head>
+<body>
+<header class="cabecalho">
+    <h1>Curso PHP</h1>
+    <h2>Indice dos Exercícios</h2>
+</header>
+<main class="principal">
+    <div class="conteudo">
+        <nav class="modulos">
+            <div class="modulo verde">
+                <h3>Módulo 01 </h3>
+                <ul>
+                    <li><a href="exercicio.php">Exercicio A</a></li>
+                    <li><a href="#">Exercicio A</a></li>
+                    <li><a href="#">Exercicio A</a></li>
+                </ul>
+            </div>
+            <div class="modulo vermelho">
+                <h3>Módulo 02 </h3>
+                <ul>
+                    <li><a href="#">Exercicio A</a></li>
+                    <li><a href="#">Exercicio A</a></li>
+                    <li><a href="#">Exercicio A</a></li>
+                </ul>
+            </div>
+            <div class="modulo azul">
+                <h3>Módulo 03 </h3>
+                <ul>
+                    <li><a href="#">Exercicio A</a></li>
+                    <li><a href="#">Exercicio A</a></li>
+                    <li><a href="#">Exercicio A</a></li>
+                </ul>
+            </div>
+            <div class="modulo roxo">
+                <h3>Módulo 04 </h3>
+                <ul>
+                    <li><a href="#">Exercicio A</a></li>
+                    <li><a href="#">Exercicio A</a></li>
+                    <li><a href="#">Exercicio A</a></li>
+                </ul>
+            </div>
+            <div class="modulo laranja">
+                <h3>Módulo 05 </h3>
+                <ul>
+                    <li><a href="#">Exercicio A</a></li>
+                    <li><a href="#">Exercicio A</a></li>
+                    <li><a href="#">Exercicio A</a></li>
+                </ul>
+            </div>
+            <div class="modulo verde-escuro">
+                <h3>Módulo 06 </h3>
+                <ul>
+                    <li><a href="#">Exercicio A</a></li>
+                    <li><a href="#">Exercicio A</a></li>
+                    <li><a href="#">Exercicio A</a></li>
+                </ul>
+            </div>
+            <div class="modulo vermelho-escuro">
+                <h3>Módulo 07 </h3>
+                <ul>
+                    <li><a href="#">Exercicio A</a></li>
+                    <li><a href="#">Exercicio A</a></li>
+                    <li><a href="#">Exercicio A</a></li>
+                </ul>
+            </div>
+            <div class="modulo azul-escuro">
+                <h3>Módulo 08 </h3>
+                <ul>
+                    <li><a href="#">Exercicio A</a></li>
+                    <li><a href="#">Exercicio A</a></li>
+                    <li><a href="#">Exercicio A</a></li>
+                </ul>
+            </div>
+            <div class="modulo roxo-escuro">
+                <h3>Módulo 09 </h3>
+                <ul>
+                    <li><a href="#">Exercicio A</a></li>
+                    <li><a href="#">Exercicio A</a></li>
+                    <li><a href="#">Exercicio A</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</main>
+<footer class="rodape">
+    Gadotti© <?= date('Y')?>
+</footer>
+</body>
+</html>
